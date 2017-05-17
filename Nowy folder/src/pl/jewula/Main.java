bf0191028b3070ee5f26b3730ca8933426196125
Aleksander Jewuła <chuddyni@gmail.com>
2017-05-17 21:31:12 +0200

    Test2

--- Nowy folder/src/pl/jewula/Main.java
+++ Nowy folder/src/pl/jewula/Main.java
@@ -1,8 +1,94 @@
 package pl.jewula;
 
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        //ssl fix
+
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+        }
+
+        try {
+            URL url = new URL("https://backoffice.miceolution.eu/");
+        } catch (MalformedURLException e) {
+        }
+
+
+        PrintWriter out = new PrintWriter("69name.txt");
+
+        for (int i = 2323; i < 2330; i++) {
+            try {
+
+                //connect
+
+                System.out.printf("\n");
+                Document doc = Jsoup.connect("https://backoffice.miceolution.eu/en/user/" + i).get();
+
+
+
+                Element content = doc.getElementsByClass("username active").last();
+                Elements links = content.getElementsByTag("a");
+
+                for (Element link : links) {
+                    String linkHref = link.attr("href");
+
+                  //  System.out.print(linkHref);
+
+                    String linkText = link.text();
+
+                    System.out.println(linkText);
+                }
+
+
+//                String strona_dane = doc.text();
+//                System.out.println(strona_dane);
+//
+//                out.println(strona_dane + "\n");
+
+            } catch (NullPointerException e) {
+            } catch (HttpStatusException e) {
+            } catch (IOException e) {
+            }
+
+        }
+
     }
+
 }
